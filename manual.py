ddf7b60789dc5d82e86377aab0d6296189283767
Amadeusz Wieczorek <git@amadeusw.com>
2025-02-11 21:34:20 -0800

    got a good set of controls

--- manual.py
+++ manual.py
@@ -1,32 +1,57 @@
 import cv2
 import numpy as np
 
+# Define a class to hold image data
+class ImageData:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.image = cv2.imread(filepath)
+        self.translationX = 0
+        self.translationY = 0
+        self.rotation = 0
+        self.skew = 0
+        self.zoom = 0
+
+# Initialize list of images instead of individual variables
 useEdgeDetection = False
-xTranslationA = 0
-yTranslationA = 0
-xTranslationB = 0
-yTranslationB = 0
+useBlending = False
+currentIndex = 0
 deltaTranslation = 20
-imageA = cv2.imread('data/watertower01.jpg')
-imageB = cv2.imread('data/watertower02.jpg')
-visibleImageA = imageA
-visibleImageB = imageB
+edgeA = 100
+edgeB = 200
+images = [
+    ImageData('data/watertower01.jpg'),
+    ImageData('data/watertower02.jpg'),
+    ImageData('data/watertower03.jpg')
+]
+
+def getImage(index):
+    imageData = images[index]
+    print("using ", index, imageData.filepath)
+    image = cv2.Canny(imageData.image, edgeA, edgeB) if useEdgeDetection else imageData.image
+    # Create transformation matrix incorporating translation, rotation and skew
+    angle = np.radians(imageData.rotation)
+    cos_a = np.cos(angle)
+    sin_a = np.sin(angle)
+    skew_rad = np.radians(imageData.skew)
+    transform_matrix = np.float32([
+        [cos_a, -sin_a + np.tan(skew_rad), imageData.translationX],
+        [sin_a, cos_a, imageData.translationY]
+    ])
+    transform_matrix = transform_matrix * (1 + imageData.zoom)
+    transformed = cv2.warpAffine(image, transform_matrix,
+                                   (imageData.image.shape[1], imageData.image.shape[0]))
+    return transformed
 
 def updateImages():
-    if useEdgeDetection:
-        visibleImageA = cv2.Canny(imageA, 100, 200)
-        visibleImageB = cv2.Canny(imageB, 100, 200)
+    print("update. blending ", useBlending)
+    if useBlending:
+        return cv2.addWeighted(
+            getImage(currentIndex), 0.5,
+            getImage((currentIndex - 1) % len(images)), 0.5,
+            0)
     else:
-        visibleImageA = imageA
-        visibleImageB = imageB
-
-    print("edge detection", useEdgeDetection)    
-    translation_matrixA = np.float32([[1, 0, xTranslationA], [0, 1, yTranslationA]])
-    translation_matrixB = np.float32([[1, 0, xTranslationB], [0, 1, yTranslationB]])
-    visibleImageA = cv2.warpAffine(visibleImageA, translation_matrixA, (imageA.shape[1], imageA.shape[0]))
-    visibleImageB = cv2.warpAffine(visibleImageB, translation_matrixB, (imageB.shape[1], imageB.shape[0]))
-    blended = cv2.addWeighted(visibleImageA, 0.5, visibleImageB, 0.5, 0)
-    return blended
+        return getImage(currentIndex)
 
 def updateWindow(image):
     window_name = 'Art Star'
@@ -44,29 +69,55 @@ def update():
     finalImage = updateImages()
     updateWindow(finalImage)
 
+def constrain_value(value, delta, min_val, max_val):
+    new_value = value + delta
+    return max(min_val, min(new_value, max_val))
+
+update()
 while True:
     key = cv2.waitKey(0)
-    match (chr(key & 0xFF) if key != -1 else '\0', bool(key & 0xFF00)):  # (key char, is_ctrl_pressed)
-        case 'e', False:
+    if key == -1:
+        continue
+
+    match chr(key & 0xFF):
+        case 'E':
             useEdgeDetection = not useEdgeDetection
-        case 'q', False:
+        case 'B':
+            useBlending = not useBlending
+        case 'Q':
             break
-        case ('w', True):
-            yTranslationB -= deltaTranslation
-        case ('w', False):
-            yTranslationA -= deltaTranslation
-        case ('s', True):
-            yTranslationB += deltaTranslation
-        case ('s', False):
-            yTranslationA += deltaTranslation
-        case ('a', True):
-            xTranslationB -= deltaTranslation
-        case ('a', False):
-            xTranslationA -= deltaTranslation
-        case ('d', True):
-            xTranslationB += deltaTranslation
-        case ('d', False):
-            xTranslationA += deltaTranslation
+        case '=':
+            currentIndex = (currentIndex + 1) % len(images)
+        case '-':
+            currentIndex = (currentIndex - 1) % len(images)
+        case '[':
+            edgeA = constrain_value(edgeA, -10, 0, 255)
+        case ']':
+            edgeA = constrain_value(edgeA, 10, 0, 255)
+        case '{':
+            edgeB = constrain_value(edgeB, -10, 0, 255)
+        case '}':
+            edgeB = constrain_value(edgeB, 10, 0, 255)
+        case 'r':
+            images[currentIndex].skew = constrain_value(images[currentIndex].skew, -1, -89, 89)
+        case 'w':
+            images[currentIndex].translationY -= deltaTranslation
+        case 'f':
+            images[currentIndex].skew = constrain_value(images[currentIndex].skew, 1, -89, 89)
+        case 's':
+            images[currentIndex].translationY += deltaTranslation
+        case 'q':
+            images[currentIndex].rotation = constrain_value(images[currentIndex].rotation, -1, -180, 180)
+        case 'a':
+            images[currentIndex].translationX -= deltaTranslation
+        case 'e':
+            images[currentIndex].rotation = constrain_value(images[currentIndex].rotation, 1, -180, 180)
+        case 'd':
+            images[currentIndex].translationX += deltaTranslation
+        case 'z':
+            images[currentIndex].zoom -= 0.05
+        case 'x':
+            images[currentIndex].zoom += 0.05
     update()
 
 cv2.destroyAllWindows()
